39e0adc6d6ebc498a17dc7cc4a8da75003dbb4a5
Eve Yan <yanchuaner@gmail.com>
2012-04-20 14:36:53 +0800

    first commit

---
+++ BowlingGameTest/BowlingGameTest/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGameTest
+{
+    class Game
+    {
+        private int nscore = 0;
+        private int[] rolls = new int[21];
+        private int currentOne = 0;
+
+        public void roll(int pins)
+        {
+            rolls[currentOne++] = pins;
+        }
+
+        public int score()
+        {
+            nscore = 0;
+            for (int loop = 0; loop < currentOne; ++loop)
+            {
+                nscore += rolls[loop];
+                if (isStrike(loop))
+                {
+                    nscore += rolls[loop + 1];
+                    nscore += rolls[loop + 2];
+                }
+                else if (isSpare(loop))
+                {
+                    nscore += rolls[loop + 1];
+                    nscore += rolls[loop + 2];
+                    loop += 1;
+                }
+
+            }
+            return nscore;
+        }
+
+        private bool isStrike(int loop)
+        {
+            return (rolls[loop] == 10 && loop < currentOne - 3);
+        }
+
+        private Boolean isSpare(int loop)
+        {
+            return (loop <= currentOne - 1 && rolls[loop] + rolls[loop + 1] == 10);
+           
+        }
+    }
+}
---
+++ BowlingGameTest/BowlingGameTest/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Game;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Game game = new Game();
+
+
+        private void RollMany(int times, int score)
+        {
+            for (int loop = 0; loop < times; ++loop)
+            {
+                game.roll(score);
+            }
+        }
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            game.roll(3);
+            game.roll(4);
+
+            RollMany(16, 0);
+
+            Assert.AreEqual(game.score(), 24);
+        }
+        [TestMethod]
+        public void TestOneSpareStrike()
+        {
+            RollStrike();
+            RollSpare();
+            game.roll(4);
+
+            RollMany(15, 0);
+
+            Assert.AreEqual(game.score(), 38);
+        }
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            game.roll(3);
+            game.roll(4);
+
+            RollMany(16, 0);
+
+            Assert.AreEqual(game.score(), 20);
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+
+            Assert.AreEqual(game.score(), 300);
+        }
+        private void RollStrike()
+        {
+            game.roll(10);
+        }
+
+        private void RollSpare()
+        {
+            game.roll(5);
+            game.roll(5);
+        }
+
+        [TestMethod]
+        public void Test20Zero()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(game.score(), 0);
+        }
+
+        [TestMethod]
+        public void Test20One()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(game.score(), 20);
+        }
+    }
+}
